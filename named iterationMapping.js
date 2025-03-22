186ae48a72fd26e1e1ebefae1bb6e9c0ddf1e059
HACHILES, MATHEW EDGIE H. <mathewedgieh@gmail.com>
2025-03-22 19:30:34 +0800

    initial commit

---
+++ named iterationMapping.js
@@ -0,0 +1,10 @@
+let maxLimit = parseInt(prompt("Enter the Limit of the Double Loop:"));
+
+console.log("Result:");
+for (let i = 0; i <= maxLimit; i++) {
+  for (let j = 0; j <= maxLimit; j++) {
+    let addedValue = i + j;
+
+    console.log(`[${i}] [${j}] Added value is ${addedValue}`);
+  }
+}
